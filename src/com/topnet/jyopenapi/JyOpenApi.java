e05d8cd5944752778f3a497238c29c55e154bcd7
renzheng <renzheng@topnet.net.cn>
2018-02-09 17:02:25 +0800

    init

---
+++ src/com/topnet/jyopenapi/JyOpenApi.java
@@ -0,0 +1,41 @@
+package com.topnet.jyopenapi;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Described 剑鱼数据开放平台接口调用样例
+ * @author rz
+ * @date 2018-02-09
+ * @version v1.0
+ */
+public class JyOpenApi  {
+	
+	/**
+	 * 调用样例
+	 * 
+	 */
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String appid="jyo_2018Yhsal8Ujk";
+		String secret="secRtjdsklv90bbn";
+		
+		Map<String, String> parameters = new HashMap<String, String>();
+		// 请求参数
+		parameters.put("action","getdata");
+	    parameters.put("key", "北京拓普丰联");
+	    parameters.put("appid", appid);
+	    parameters.put("timestamp", new Date().getTime()/1000+"");
+  
+	    //参数签名
+	    try {
+	    	String signature=JyOpenUtils.Signature(parameters, secret);
+	    	parameters.put("signature",signature);
+	    	JyOpenUtils.post("https://api.jianyu360.com/open",parameters);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
